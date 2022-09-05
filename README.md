0a6e0de3283f3d92ab91b78aa4448a83c8b30792
bw <bw@dev.null>
2022-09-05 01:28:46 -0500

    getting queue working, adding logic for populating queue with users and scores

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # open-etl
 Open source implementations and architecture for different ETL workloads.
+
+## sqs
+
+to setup for first time create a queue:
+`aws --endpoint http://localhost:4556 sqs create-queue --queue-name results-queue --attributes MessageRetentionPeriod=1209600,ContentBasedDeduplication=false`
--- results-queue-gen/src/server.py
+++ results-queue-gen/src/server.py
@@ -1,28 +1,66 @@
+import boto3
+import json
 from collections import namedtuple
 
-filepath = '/data/IPIP300.dat'
 
-ParsedRow = namedtuple('ParsedRow', 'case_id sex age country answers')
-positions = {
-    'case_id': [0, 6],
-    'sex': [6, 7],
-    'age': [7, 9],
-    'country': [22, 32],
-    'answers': [33, 333]}
+def generate_message(parsed_record):
+    return json.dumps(parsed_record._asdict())
 
-ps_records = []
 
-with open(filepath) as fp:
-    for cnt, line in enumerate(fp):
-       parsed = ParsedRow(
-           case_id=line[0:6].strip(),
-           sex=line[6:7].strip(),
-           age=line[7:9].strip(),
-           country=line[22:32].strip(),
-           answers=line[33:333].strip()
-       )
-       ps_records.append(parsed)
+def send_message(sqs_client, sqs_queue_url, answer_id, message):
+    response = sqs_client.send_message(
+        QueueUrl=sqs_queue_url,
+        DelaySeconds=10,
+        MessageAttributes={
+            'Title': {
+                'DataType': 'String',
+                'StringValue': answer_id
+            }
+        },
+        MessageBody=(
+            message
+        )
+    )
+    return response
 
-print(len(ps_records))
-# for ps in ps_records:
-#     print(len(ps_records))
+
+def main():
+    # TODO: switch to env var
+    filepath = '/Users/mydev/code/open-etl/results-queue-gen/data/IPIP300.dat'
+    ParsedRow = namedtuple('ParsedRow', 'case_id sex age country answers')
+    positions = {
+        'case_id': [0, 6],
+        'sex': [6, 7],
+        'age': [7, 9],
+        'country': [22, 32],
+        'answers': [33, 333]}
+
+    ps_records = []
+
+    with open(filepath) as fp:
+        for cnt, line in enumerate(fp):
+            parsed = ParsedRow(
+                case_id=line[0:6].strip(),
+                sex=line[6:7].strip(),
+                age=line[7:9].strip(),
+                country=line[22:32].strip(),
+                answers=line[33:333].strip()
+            )
+            ps_records.append(parsed)
+
+    aws_url = "http://localhost:4566"
+    queue_name = 'results-queue'
+    queue_url = "{}/000000000000/{}".format(aws_url, queue_name)
+    aws_client = boto3.client('sqs', endpoint_url=aws_url)
+
+    for record in ps_records:
+        answer_id = record.case_id
+        msg = generate_message(record)
+
+        # ENV VAR
+        response = send_message(aws_client, queue_url, answer_id, generate_message(msg))
+        print("sent message: msg_id: {} answer_id: {}".format(response['MessageId'], answer_id))
+
+
+if __name__ == "__main__":
+    main()
